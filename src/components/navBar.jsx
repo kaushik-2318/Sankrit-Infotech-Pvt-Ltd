357bc7af3841d5eb9dc38da378e3d5ca16dc3026
kaushik-2318 <kaushikverma321@gmail.com>
2024-06-24 19:05:09 +0530

    NavBar Bug Remove

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -10,6 +10,9 @@ function NavBar() {
   const toggle = () => {
     setsidepanel(!sidepanel);
   };
+  const logotoggle = () =>{
+    setsidepanel(false);
+  }
   useGSAP(() => {
     gsap.from('.nava', {
       y: -30,
@@ -28,15 +31,15 @@ function NavBar() {
     <div className='relative flex justify-center'>
       <div className='fixed text-white rounded-3xl m-4 p-4 px-8 w-[97%] border-2 z-20 backdrop-blur-sm shadow-xl'>
         <div className='flex flex-row justify-between items-center'>
-          <Link to="home" spy={true} duration={1000} activeClass="a" smooth={true}><div className='navside cursor-pointer text-xl font-bold'>Sankrit Infotech Pvt Ltd.</div></Link>
+          <Link to="home" spy={true} duration={1000} activeClass="a" onClick={logotoggle} smooth={true}><div className='navside cursor-pointer text-xl font-bold'>Sankrit Infotech Pvt Ltd.</div></Link>
           <div className='hidden md:block'>
             <ul className='flex flex-row gap-7'>
               <Link to="home" spy={true} duration={1000} smooth={true} activeClass="active" className='nava hover:border-b-2 cursor-pointer'><li>Home</li></Link>
               <Link to="about" spy={true} offset={-50} duration={1000} activeClass="active" smooth={true} className='nava hover:border-b-2 cursor-pointer'><li>About</li></Link>
-              <Link to="services" spy={true} duration={1000} smooth={true} activeClass="active" className='nava cursor-pointer hover:border-b-2'><li>Services</li></Link>
-              <Link to="portfolio" spy={true} duration={1000} smooth={true} activeClass="active" className='nava cursor-pointer hover:border-b-2'><li>Portfolio</li></Link>
-              <Link to="team" spy={true} duration={1000} smooth={true} activeClass="active" className='nava cursor-pointer hover:border-b-2'><li>Team</li></Link>
-              <Link to="contact" spy={true} duration={1000} smooth={true} activeClass="active" className='nava cursor-pointer hover:border-b-2'><li>Contact</li></Link>
+              <Link to="services" spy={true} duration={1000} smooth={true} offset={70} activeClass="active" className='nava cursor-pointer hover:border-b-2'><li>Services</li></Link>
+              <Link to="portfolio" spy={true} duration={1000} smooth={true} offset={-30} activeClass="active" className='nava cursor-pointer hover:border-b-2'><li>Portfolio</li></Link>
+              <Link to="team" spy={true} duration={1000} smooth={true} activeClass="active" offset={-30} className='nava cursor-pointer hover:border-b-2'><li>Team</li></Link>
+              <Link to="contact" spy={true} duration={1000} smooth={true} activeClass="active" offset={-50} className='nava cursor-pointer hover:border-b-2'><li>Contact</li></Link>
             </ul>
           </div>
           <div className='block md:hidden'>
@@ -47,12 +50,12 @@ function NavBar() {
       <div className={`side fixed h-screen z-10 flex justify-center items-center backdrop-blur-lg shadow-xl flex-col transition-all ease-in-out duration-500 md:hidden ${sidepanel ? `left-0` : `left-96 hidden`}`}>
         <div className=''>
           <ul className='flex h-screen justify-center items-center flex-col w-screen'>
-            <Link to='home' spy={true} duration={1000} activeClass="active" smooth={true}><li className='px-24 py-5 hover:border-b-white hover:border-b-2' >Home</li></Link>
-            <Link to='about' spy={true} offset={-50} duration={1000} activeClass="active" smooth={true}><li className='px-24 py-5 hover:border-b-white hover:border-b-2' >About</li></Link>
-            <Link to='services' spy={true} duration={1000} activeClass="active" smooth={true}><li className='px-24 py-5 hover:border-b-white hover:border-b-2' >Services</li></Link>
-            <Link to='portfolio' spy={true} duration={1000} activeClass="active" smooth={true}><li className='px-24 py-5 hover:border-b-white hover:border-b-2' >Portfolio</li></Link>
-            <Link to='team' spy={true} duration={1000} activeClass="active" smooth={true}><li className='px-24 py-5 hover:border-b-white hover:border-b-2' >Team</li></Link>
-            <Link to='contact' spy={true} duration={1000} activeClass="active" smooth={true}><li className='px-24 py-5 hover:border-b-white hover:border-b-2' >Contact</li></Link>
+            <Link to='home' spy={true} duration={1000} activeClass="active" onClick={toggle} smooth={true}><li className='px-24 py-5 hover:border-b-white hover:border-b-2' >Home</li></Link>
+            <Link to='about' spy={true} offset={-40} duration={1000} onClick={toggle} activeClass="active" smooth={true}><li className='px-24 py-5 hover:border-b-white hover:border-b-2' >About</li></Link>
+            <Link to='services' spy={true} duration={1000} activeClass="active" offset={70} onClick={toggle} smooth={true}><li className='px-24 py-5 hover:border-b-white hover:border-b-2' >Services</li></Link>
+            <Link to='portfolio' spy={true} offset={-25} duration={1000} activeClass="active" onClick={toggle} smooth={true}><li className='px-24 py-5 hover:border-b-white hover:border-b-2' >Portfolio</li></Link>
+            <Link to='team' spy={true} duration={1000} offset={-25} activeClass="active" onClick={toggle} smooth={true}><li className='px-24 py-5 hover:border-b-white hover:border-b-2' >Team</li></Link>
+            <Link to='contact' spy={true} duration={1000} offset={-45} activeClass="active" onClick={toggle} smooth={true}><li className='px-24 py-5 hover:border-b-white hover:border-b-2' >Contact</li></Link>
           </ul>
         </div>
       </div>
